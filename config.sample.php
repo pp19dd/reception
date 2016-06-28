8188c95fd857a465e20a5ad7e233a6ce10444fa8
pp19dd <pp19dd@gmail.com>
2016-06-28 14:39:27 -0400

    Add database info to config

--- config.sample.php
+++ config.sample.php
@@ -1,2 +1,5 @@
 <?php
 define( "MAP_API_KEY", "" );
+define( "PDO_CONN",         "mysql:host=127.0.0.1; dbname=dbname; charset=utf8" );
+define( "PDO_USER",         "dbuser" );
+define( "PDO_PASS",         "dbpass" );
