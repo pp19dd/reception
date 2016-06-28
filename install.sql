3422217aeb07521b4abe358cb6d60fca4628e2f2
pp19dd <pp19dd@gmail.com>
2016-06-28 14:35:45 -0400

    Store basics for aggregation

---
+++ install.sql
@@ -0,0 +1,13 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE IF NOT EXISTS `entries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stamp` datetime NOT NULL,
+  `reception` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `lat` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `lng` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `zoom` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `ip` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
